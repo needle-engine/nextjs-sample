16f3f90cc72bcc9ad5085498bbf1341f54fa9b79
Marcel Wiessler <marcel@gaisterhand.de>
2023-05-18 14:12:18 +0200

    needle engine element types

--- src/needleEngine.tsx
+++ src/needleEngine.tsx
@@ -1,17 +1,41 @@
 // https://nextjs.org/docs/getting-started/react-essentials#the-use-client-directive
 'use client'
-import "./generated/gen";
+
 const isServer = () => typeof window === `undefined`;
 
-// client only
 import "@needle-tools/engine"
+import { NeedleEngineAttributes } from "@needle-tools/engine";
+import { useEffect, useState } from "react";
 
+export type NeedleEngineProps = NeedleEngineAttributes;
 
-export default function NeedleEngine({ ...props }) {
+/** **Needle Engine Component**  
+ * Import with `const NeedleEngine = dynamic(() => import('./needleEngine'), { ssr: false })`
+ * 
+ * @example
+ * <NeedleEngine src="./assets/next.glb" loading-style="light" style={{ width: '100%', height: '100%', }}></NeedleEngine>
+ * 
+ */
+export default function NeedleEngine({ ...props }): JSX.Element {
+
+  const [src, setSrc] = useState(props?.src)
+
+  useEffect(() => {
+    // lazy import the codegen if no explicit src is defined
+    if (!isServer() && props?.src === undefined) {
+      import("./generated/gen")
+        .then((m) => {
+          setSrc(m.needle_exported_files)
+        })
+        .catch((e) => {
+          console.error(e)
+        });
+    }
+  }, [])
 
   return (
     <>
-      {!isServer() && <needle-engine {...props}></needle-engine>}
+      {!isServer() && <needle-engine src={src} {...props}></needle-engine>}
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 import dynamic from 'next/dynamic'
-const NeedleEngine = dynamic(() => import('../needleEngine'), { ssr: false })
+
+import { type NeedleEngineProps } from '../needleEngine'
+const NeedleEngine = dynamic<NeedleEngineProps>(() => import('../needleEngine'), { ssr: false })
+
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -37,13 +40,14 @@ export default function Home() {
       </div>
 
       <div className="relative flex items-center justify-between w-full h-[48rem] lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 my-5">
-        <NeedleEngine 
-        loading-style="light"
-        style={{
-          width: '100%',
-          height: '100%',
-        }}>
-        </NeedleEngine>
+
+        <NeedleEngine
+          loading-style="light"
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
