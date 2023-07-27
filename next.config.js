5c29eff6169f44761f555a1611f2857f4fa1a71f
hybridherbst <felix.herbst@gmail.com>
2023-07-27 16:31:42 +0200

    add .js/.ts remapping in config (for local dev in TS only)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const webpack = require("webpack");
 
 module.exports = async () => {
   const { needleNext } = await import("@needle-tools/engine/plugins/next/index.js");
-  return needleNext({}, { modules: { webpack } });
+  return needleNext({webpack: (config) => {
+    config.resolve.extensionAlias = {
+      ".js": [".js", ".ts"],
+  };
+  return config;
+  }}, { modules: { webpack } });
 }
-
