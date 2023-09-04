845d833a207aae275d69cfc503766f1c8b970005
Marcel Wiessler <marcel@gaisterhand.de>
2023-09-04 15:01:33 +0200

    Fix Needle Engine import

--- src/needleEngine.tsx
+++ src/needleEngine.tsx
@@ -3,8 +3,7 @@
 
 const isServer = () => typeof window === `undefined`;
 
-import "@needle-tools/engine"
-import { NeedleEngineAttributes } from "@needle-tools/engine";
+import { type NeedleEngineAttributes } from "@needle-tools/engine";
 import { useEffect, useState } from "react";
 
 // Make addEventListener optional
