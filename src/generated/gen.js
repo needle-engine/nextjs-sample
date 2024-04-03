8588f192965c5191db37e62fe33cec6dbb5a3c7c
Krystof <kipash612@gmail.com>
2024-04-03 17:35:40 +0200

    samples-update-16

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -1,17 +1,15 @@
-﻿import "./register_types.ts"
+﻿globalThis["needle:dependencies:ready"] = import("./register_types.ts")
 
-export const needle_exported_files = [];
+export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("./assets/next.glb?v=1684403368979");
+needle_exported_files.push("./assets/next.glb?v=1712158385970");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1684403368979");
+		needleEngine.setAttribute("hash", "1712158385970");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
-
-console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools");
