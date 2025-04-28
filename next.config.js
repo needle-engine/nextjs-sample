7a33b9cb93615023c51e1f0853ad5dbced57a206
Marcel Wiessler <marcel@gaisterhand.de>
2025-04-28 22:55:11 +0700

    add .env and NEEDLE_CLOUD_TOKEN + update NE

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const webpack = require("webpack");
+const dotenv = require("dotenv");
 
 module.exports = async () => {
+  // Load environment variables from .env file
+  dotenv.config();
+
   const { needleNext } = await import("@needle-tools/engine/plugins/next/index.js");
   return needleNext({webpack: (config) => {
     config.resolve.extensionAlias = {
